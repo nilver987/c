3881af2961905d6649d8097fa5164d7ed6691f8f
nilver987 <nilver.1967@gmail.com>
2024-09-05 08:35:20 -0500

    first commit

---
+++ ms-cliente-service/src/main/java/org/example/msclienteservice/MsClienteServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.msclienteservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsClienteServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsClienteServiceApplication.class, args);
+    }
+
+}
